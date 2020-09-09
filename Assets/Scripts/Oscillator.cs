5428c51c6b96970ee494256c407c71a41cb6b456
Eric R <eruxton@outlook.com>
2020-09-09 00:35:39 -0400

    Project boost about half done already

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(0f, 15f, 0f);
+    [SerializeField] float period = 2f;
+    [Range(0,1)] [SerializeField] float movementFactor;
+    Vector3 startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float sinWave = Mathf.Sin(cycles * tau);
+        movementFactor = sinWave / 2f + 0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPosition + offset;
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rocketBody;
+    AudioSource rocketSound;
+    [SerializeField] float rcsThrust = 150f;
+    [SerializeField] float mainThrust = 150f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explosionSound;
+    [SerializeField] AudioClip victorySound;
+    [SerializeField] ParticleSystem engineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem defeatParticles;
+    enum State { Alive, Dying, Transcending}
+    State currentState = State.Alive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketBody = GetComponent<Rigidbody>();
+        rocketSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (currentState != State.Alive)
+        {
+            return;
+        }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                break;
+            case "Finish":
+                //load next scene
+                currentState = State.Transcending;
+                successParticles.Play();
+                rocketSound.Stop();
+                rocketSound.PlayOneShot(victorySound);
+                Invoke("LoadNextScene", 1f);
+                break;
+            default:
+                //die
+                currentState = State.Dying;
+                defeatParticles.Play();
+                rocketSound.Stop();
+                rocketSound.PlayOneShot(explosionSound);
+                Invoke("LoadFirstScene", 1.0f);
+                break;
+        }
+    }
+
+    private void LoadFirstScene()
+    {
+        currentState = State.Alive;
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        currentState = State.Alive;
+        SceneManager.LoadScene(1);
+    }
+
+    private void Rotate()
+    {
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+        rocketBody.freezeRotation = true; // this makes it so that it only rotates through player input and not by hitting objects
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+        rocketBody.freezeRotation = false; //hitting things now makes the rocket turn again
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!rocketSound.isPlaying)
+            {
+                rocketSound.PlayOneShot(mainEngine);
+                engineParticles.Play();
+            }
+            rocketBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+        else
+        {
+            rocketSound.Stop();
+            engineParticles.Stop();
+        }
+    }
+}
